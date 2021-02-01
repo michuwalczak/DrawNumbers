9f5eadf72282f7177b0e9badffc5e3d5b59f82cb
michuwalczak <michuwalczak@gmail.com>
2021-02-01 18:05:16 +0100

    Database helper

---
+++ DrawNumbers.DAL/DataBaseHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DrawNumbers.DAL
+{
+    /// <summary>
+    /// Manages access of database content
+    /// </summary>
+    public class DataBaseHelper
+    {
+        private readonly DatabaseEntities context;
+        private readonly object drawnNumberLock = new object();
+
+        /// <summary>
+        /// Get or add drawn numbers
+        /// </summary>
+        public List<DrawnNumber> DrawnNumbers
+        {
+            get
+            {
+                lock (drawnNumberLock)
+                {
+                    return context.DrawnNumber.ToList();
+                };
+            }
+
+            private set
+            {
+                lock (drawnNumberLock)
+                {
+                    context.DrawnNumber.AddRange(value);
+                }
+            }
+        }
+
+        public DataBaseHelper()
+        {
+            context = new DatabaseEntities();
+            
+            // TEST !!!!!!!!!!!!!
+            RemoveAll();
+        }
+
+
+        /// <summary>
+        /// Add number to data base
+        /// </summary>
+        /// <param name="number"></param>
+        public void Add(DrawnNumber number)
+        {
+            this.DrawnNumbers = new List<DrawnNumber> { number };
+            Console.WriteLine($"Added number: {number.Value}");
+        }
+
+        /// <summary>
+        /// Save all changes in database
+        /// </summary>
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Remove all numbers from database
+        /// </summary>
+        public void RemoveAll()
+        {
+            var data = from n in context.DrawnNumber
+                       select n;
+            foreach (var number in data)
+                context.DrawnNumber.Remove(number);
+
+            context.SaveChanges();
+        }
+    }
+}
